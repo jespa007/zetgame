3b986f38797d1fa78daba40d98805008cf43f7bb
Jordi Espada <jespa007@gmail.com>
2021-05-30 23:52:24 +0200

    last changes

--- ecs/zg_ecs.c
+++ ecs/zg_ecs.c
@@ -1,11 +1,12 @@
 
 #include "component/ECComponent.c"
 #include "component/ECTweenTransform.c"
-#include "component/ECMaterial.c"
-#include "component/ECTexture.c"
 #include "component/ECCamera.c"
 #include "component/ECRenderable.c"
-#include "component/ECTransform.c"
+#include "component/ECViewer2d.c"
+#include "component/ECText2d.c"
+#include "component/ECRenderable.c"
+
 #include "entity/Entity.c"
 //#include "entity/EntityType.c"
 #include "system/ESSystem.c"
