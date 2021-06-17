c2f8e01f5ce9209d254314023a437583174e47c2
jespa007 <jespa007@gmail.com>
2021-06-17 22:32:31 +0200

    last changes

--- physics/2d/Physics2dCollider.h
+++ physics/2d/Physics2dCollider.h
@@ -3,12 +3,19 @@
 
 typedef struct Physics2dCollider Physics2dCollider;
 
+typedef enum{
+	 PHYSICS2D_COLLIDER_QUAD=0
+	,PHYSICS2D_COLLIDER_CIRCLE
+
+}Physics2dCollider;
+
 struct Physics2dCollider{
 	void *data;
 };
 
 
-Physics2dCollider * Physics2dCollider_New(void);
+Physics2dCollider * Physics2dCollider_NewQuad(uint16_t width, uint16_t height);
+Physics2dCollider * Physics2dCollider_NewCircle(uint16_t diameter);
 void Physics2dCollider_Delete(Physics2dCollider * _this);
 
 
