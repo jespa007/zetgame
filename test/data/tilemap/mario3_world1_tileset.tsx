473a7d8b583e744cb3b5c78af6f4345e51c82b14
Jordi Espada <jespa007@gmail.com>
2021-10-28 00:14:03 +0200

    last changes

--- test/data/tilemap/mario3_world1_tileset.tsx
+++ test/data/tilemap/mario3_world1_tileset.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="mario3_world1_tileset" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+<tileset version="1.5" tiledversion="1.7.2" name="mario3_world1_tileset" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="49" columns="7">
  <image source="mario3_world1_tileset.png" width="128" height="128"/>
  <tile id="0">
   <animation>
