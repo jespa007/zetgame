fd201f7824de7580d8abcbd923d00622a3585962
jespada <jespa007@gmail.com>
2024-02-03 19:04:25 +0100

    update rpi demo

--- test/test_window_rpi.c
+++ test/test_window_rpi.c
@@ -6,6 +6,8 @@
 #include "GLES2/gl2.h"
 #include "EGL/egl.h"
 #include "EGL/eglext.h"
+#include <fcntl.h>
+#include <linux/input.h>
 
 typedef struct{
 	DISPMANX_DISPLAY_HANDLE_T 	dispmanx_display;
@@ -15,6 +17,56 @@ typedef struct{
 }Window_RPI;
 
 #define check() assert(glGetError() == 0)
+
+typedef struct{
+	int fd;
+}Input_RPI;
+
+// Define the path to the input event device
+#define INPUT_EVENT_DEVICE "/dev/input/event0"
+
+void Input_RPI_Create(Input_RPI *_input_rpi){
+
+	memset(_input_rpi,0,sizeof(Input_RPI));
+
+    // Open the input event device
+    int fd = open(INPUT_EVENT_DEVICE, O_RDONLY);
+    if (fd < 0) {
+        exit(EXIT_FAILURE);
+    }
+
+}
+
+bool pressed_esc=false;
+
+
+void Input_RPI_Update(Input_RPI *_input_rpi){
+	 struct input_event ev;
+	ssize_t bytes = read(_input_rpi->fd, &ev, sizeof(ev));
+	if (bytes == sizeof(ev)) {
+		printf("Print key\n");
+		// Check if the event is a key press event and if the ESC key is pressed
+		if (ev.type == EV_KEY && ev.code == KEY_ESC && ev.value == 1) {
+			printf("ESC key pressed\n");
+			pressed_esc=true;
+			// Perform actions when the ESC key is pressed
+			// For example, exit the application
+		} else if (bytes == -1) {
+            perror("Error reading input event");
+            // Exit the loop or handle the error condition
+        } else if (bytes == 0) {
+            // End of file (EOF) reached, no more events to read
+        }
+	}
+}
+
+void Input_RPI_Destroy(Input_RPI *_input_rpi){
+
+    // Cleanup
+	 close(_input_rpi->fd);
+
+
+}
 
 void Window_RPI_Create(Window_RPI *_rpi_window){
 	memset(_rpi_window,0,sizeof(Window_RPI));
@@ -79,104 +131,40 @@ void Window_RPI_Create(Window_RPI *_rpi_window){
         eglGetConfigAttrib(_rpi_window->egl_display, configs[i], EGL_RENDERABLE_TYPE, &renderable_type);
 
         // You can inspect other attributes of the configuration here
-           // For example, color depth, depth buffer size, stencil buffer size, etc.
-       	/*EGL_ALPHA_SIZE
-		Returns the number of bits of alpha stored in the color buffer.
-
-		EGL_ALPHA_MASK_SIZE
-		Returns the number of bits in the alpha mask buffer.
-
-		EGL_BIND_TO_TEXTURE_RGB
-		Returns EGL_TRUE if color buffers can be bound to an RGB texture, EGL_FALSE otherwise.
-
-		EGL_BIND_TO_TEXTURE_RGBA
-		Returns EGL_TRUE if color buffers can be bound to an RGBA texture, EGL_FALSE otherwise.
-
-		EGL_BLUE_SIZE
-		Returns the number of bits of blue stored in the color buffer.
-
-		EGL_BUFFER_SIZE
-		Returns the depth of the color buffer. It is the sum of EGL_RED_SIZE, EGL_GREEN_SIZE, EGL_BLUE_SIZE, and EGL_ALPHA_SIZE.
-
-		EGL_COLOR_BUFFER_TYPE
-		Returns the color buffer type. Possible types are EGL_RGB_BUFFER and EGL_LUMINANCE_BUFFER.
-
-		EGL_CONFIG_CAVEAT
-		Returns the caveats for the frame buffer configuration. Possible caveat values are EGL_NONE, EGL_SLOW_CONFIG, and EGL_NON_CONFORMANT.
-
-		EGL_CONFIG_ID
-		Returns the ID of the frame buffer configuration.
-
-		EGL_CONFORMANT
-		Returns a bitmask indicating which client API contexts created with respect to this config are conformant.
-
-		EGL_DEPTH_SIZE
-		Returns the number of bits in the depth buffer.
-
-		EGL_GREEN_SIZE
-		Returns the number of bits of green stored in the color buffer.
-
-		EGL_LEVEL
-		Returns the frame buffer level. Level zero is the default frame buffer. Positive levels correspond to frame buffers that overlay the default buffer and negative levels correspond to frame buffers that underlay the default buffer.
-
-		EGL_LUMINANCE_SIZE
-		Returns the number of bits of luminance stored in the luminance buffer.
-
-		EGL_MAX_PBUFFER_WIDTH
-		Returns the maximum width of a pixel buffer surface in pixels.
-
-		EGL_MAX_PBUFFER_HEIGHT
-		Returns the maximum height of a pixel buffer surface in pixels.
-
-		EGL_MAX_PBUFFER_PIXELS
-		Returns the maximum size of a pixel buffer surface in pixels.
-
-		EGL_MAX_SWAP_INTERVAL
-		Returns the maximum value that can be passed to eglSwapInterval.
-
-		EGL_MIN_SWAP_INTERVAL
-		Returns the minimum value that can be passed to eglSwapInterval.
-
-		EGL_NATIVE_RENDERABLE
-		Returns EGL_TRUE if native rendering APIs can render into the surface, EGL_FALSE otherwise.
-
-		EGL_NATIVE_VISUAL_ID
-		Returns the ID of the associated native visual.
-
-		EGL_NATIVE_VISUAL_TYPE
-		Returns the type of the associated native visual.
-
-		EGL_RED_SIZE
-		Returns the number of bits of red stored in the color buffer.
-
-		EGL_RENDERABLE_TYPE
-		Returns a bitmask indicating the types of supported client API contexts.
-
-		EGL_SAMPLE_BUFFERS
-		Returns the number of multisample buffers.
-
-		EGL_SAMPLES
-		Returns the number of samples per pixel.
-
-		EGL_STENCIL_SIZE
-		Returns the number of bits in the stencil buffer.
-
-		EGL_SURFACE_TYPE
-		Returns a bitmask indicating the types of supported EGL surfaces.
-
-		EGL_TRANSPARENT_TYPE
-		Returns the type of supported transparency. Possible transparency values are: EGL_NONE, and EGL_TRANSPARENT_RGB.
-
-		EGL_TRANSPARENT_RED_VALUE
-		Returns the transparent red value.
-
-		EGL_TRANSPARENT_GREEN_VALUE
-		Returns the transparent green value.
-
-		EGL_TRANSPARENT_BLUE_VALUE
-		Returns the transparent blue value.
+        // For example, color depth, depth buffer size, stencil buffer size, etc.
+       	/*EGL_ALPHA_SIZE:Returns the number of bits of alpha stored in the color buffer.
+		EGL_ALPHA_MASK_SIZE:Returns the number of bits in the alpha mask buffer.
+		EGL_BIND_TO_TEXTURE_RGB:Returns EGL_TRUE if color buffers can be bound to an RGB texture, EGL_FALSE otherwise.
+		EGL_BIND_TO_TEXTURE_RGBA:Returns EGL_TRUE if color buffers can be bound to an RGBA texture, EGL_FALSE otherwise.
+		EGL_BLUE_SIZE:Returns the number of bits of blue stored in the color buffer.
+		EGL_BUFFER_SIZE:Returns the depth of the color buffer. It is the sum of EGL_RED_SIZE, EGL_GREEN_SIZE, EGL_BLUE_SIZE, and EGL_ALPHA_SIZE.
+		EGL_COLOR_BUFFER_TYPE:Returns the color buffer type. Possible types are EGL_RGB_BUFFER and EGL_LUMINANCE_BUFFER.
+		EGL_CONFIG_CAVEAT:Returns the caveats for the frame buffer configuration. Possible caveat values are EGL_NONE, EGL_SLOW_CONFIG, and EGL_NON_CONFORMANT.
+		EGL_CONFIG_ID:Returns the ID of the frame buffer configuration.
+		EGL_CONFORMANT:Returns a bitmask indicating which client API contexts created with respect to this config are conformant.
+		EGL_DEPTH_SIZE:Returns the number of bits in the depth buffer.
+		EGL_GREEN_SIZE:Returns the number of bits of green stored in the color buffer.
+		EGL_LEVEL:Returns the frame buffer level. Level zero is the default frame buffer. Positive levels correspond to frame buffers that overlay the default buffer and negative levels correspond to frame buffers that underlay the default buffer.
+		EGL_LUMINANCE_SIZE:Returns the number of bits of luminance stored in the luminance buffer.
+		EGL_MAX_PBUFFER_WIDTH:Returns the maximum width of a pixel buffer surface in pixels.
+		EGL_MAX_PBUFFER_HEIGHT:Returns the maximum height of a pixel buffer surface in pixels.
+		EGL_MAX_PBUFFER_PIXELS:Returns the maximum size of a pixel buffer surface in pixels.
+		EGL_MAX_SWAP_INTERVAL:Returns the maximum value that can be passed to eglSwapInterval.
+		EGL_MIN_SWAP_INTERVAL:Returns the minimum value that can be passed to eglSwapInterval.
+		EGL_NATIVE_RENDERABLE:Returns EGL_TRUE if native rendering APIs can render into the surface, EGL_FALSE otherwise.
+		EGL_NATIVE_VISUAL_ID:Returns the ID of the associated native visual.
+		EGL_NATIVE_VISUAL_TYPE:Returns the type of the associated native visual.
+		EGL_RED_SIZE:Returns the number of bits of red stored in the color buffer.
+		EGL_RENDERABLE_TYPE:Returns a bitmask indicating the types of supported client API contexts.
+		EGL_SAMPLE_BUFFERS:Returns the number of multisample buffers.
+		EGL_SAMPLES:Returns the number of samples per pixel.
+		EGL_STENCIL_SIZE:Returns the number of bits in the stencil buffer.
+		EGL_SURFACE_TYPE:Returns a bitmask indicating the types of supported EGL surfaces.
+		EGL_TRANSPARENT_TYPE:Returns the type of supported transparency. Possible transparency values are: EGL_NONE, and EGL_TRANSPARENT_RGB.
+		EGL_TRANSPARENT_RED_VALUE:Returns the transparent red value.
+		EGL_TRANSPARENT_GREEN_VALUE:Returns the transparent green value.
+		EGL_TRANSPARENT_BLUE_VALUE:Returns the transparent blue value.
 		*/
-
         // Check if OpenGL ES 2.0 is supported
         if (renderable_type & EGL_OPENGL_ES2_BIT) {
             // Output the supported OpenGL ES version
@@ -286,14 +274,12 @@ void Window_RPI_Destroy(Window_RPI *_rpi_window){
 
 int main() {
 
-	Window_RPI rpi_window;
+	Window_RPI window_rpi;
+	Input_RPI  input_rpi;
 
-	Window_RPI_Create(&rpi_window);
+	Window_RPI_Create(&window_rpi);
+	Input_RPI_Create(&input_rpi);
 
-
-    // OpenGL ES code
-    glClearColor(0.2f, 0.4f, 0.8f, 1.0f); // Set clear color to blue
-    glClear(GL_COLOR_BUFFER_BIT);
 
     // Define vertices of the triangle
     GLfloat vertices[] = {
@@ -335,21 +321,29 @@ int main() {
     glLinkProgram(shaderProgram);
     glUseProgram(shaderProgram);
 
-    // Specify the layout of the vertex data
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+    do{
+    	// Set clear color to blue
+        glClearColor(0.2f, 0.4f, 0.8f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
-    // Draw the triangle
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-    check();
+    	// Specify the layout of the vertex data
+    	glEnableVertexAttribArray(0);
+    	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
 
-    // Swap buffers
-    eglSwapBuffers(rpi_window.egl_display, rpi_window.egl_surface);
+    	// Draw the triangle
+    	glDrawArrays(GL_TRIANGLES, 0, 3);
+    	check();
+
+    	// Swap buffers
+    	eglSwapBuffers(window_rpi.egl_display, window_rpi.egl_surface);
+
+    	Input_RPI_Update(&input_rpi);
 
     // Wait for a few seconds
-    sleep(5);
+	}while(!pressed_esc);
 
-    Window_RPI_Destroy(&rpi_window);
+    Window_RPI_Destroy(&window_rpi);
+    Input_RPI_Destroy(&input_rpi);
 
     return 0;
 }
